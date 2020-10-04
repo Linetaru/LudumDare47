0031afa95989bc0bc6d3de495d67cf8e8db0e189
Thibaut_Guerin <thibaut.guerin@edu.devinci.fr>
2020-10-04 22:34:18 +0200

    Add room counter prefab.

---
+++ Assets/--Game/Scripts/RoomCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RoomCounter : MonoBehaviour
+{
+    public uint roomNumber;
+    public TextMeshProUGUI roomCounterUI;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+            roomCounterUI.text = roomNumber.ToString();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+            roomCounterUI.text = "";
+    }
+}
