8f914bfa185883501fb73b3c4a1619544881c0a5
Thibaut_Guerin <thibaut.guerin@edu.devinci.fr>
2020-10-05 21:44:10 +0200

    Modify the level transition prefab and the start level text duration.

--- Assets/--Game/Scripts/Menus/Menus.cs
+++ Assets/--Game/Scripts/Menus/Menus.cs
@@ -7,10 +7,11 @@ using TMPro;
 public class Menus : MonoBehaviour
 {
     public TextMeshProUGUI textLevelStart;
+    public float textLevelStartDuration;
 
     private Tween tween;
     void Start()
     {
-        tween = textLevelStart.DOColor(Color.white, 2).OnComplete(() => textLevelStart.DOColor(new Color(1, 1, 1, 0), 2));
+        tween = textLevelStart.DOColor(Color.white, textLevelStartDuration / 2).OnComplete(() => textLevelStart.DOColor(new Color(1, 1, 1, 0), textLevelStartDuration / 2));
     }
 }
