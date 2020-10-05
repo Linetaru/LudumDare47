55cb0454e550c94610b351fbf3464faa0a7a6e58
Nicolas ROGE <nicolas.roge.contact@gmail.com>
2020-10-05 14:55:36 +0200

    Modification MainMenu Animation, background et timer + Modification Crédits script

--- Assets/--Game/Scripts/Menus/MainMenu.cs
+++ Assets/--Game/Scripts/Menus/MainMenu.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public string nameLevel;
     public string creditsSceneName;
+    public Text timerText;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = true;
         Time.timeScale = 1;
+        UpdateTimerText();
+    }
+
+    public void UpdateTimerText()
+    {
+        if(GeneralCountDown.instance != null)
+        {
+            GeneralCountDown.instance.text = timerText;
+            GeneralCountDown.instance.SetTimerTextOnMainMenu();
+        }
     }
 
     public void PlayGame()
